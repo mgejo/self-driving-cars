d0bc5c1cbcf467b986ab025b3f382a6176981e0d
Martin Gejo <66442848+mgejo@users.noreply.github.com>
2023-03-07 15:13:54 -0300

    add road

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50, ctx);
+const road = new Road(canvas.width / 2, canvas.width * .9, 3, ctx);
+const car = new Car(road.gateLaneCenter(1), 100, 30, 50, ctx);
 car.draw();
 
 animate();
@@ -11,6 +12,10 @@ animate();
 function animate() {
     car.update();
     canvas.height = window.innerHeight;
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * .7);
+    road.draw();
     car.draw();
+    ctx.restore();
     requestAnimationFrame(animate);
 }
---
+++ road.js
@@ -0,0 +1,58 @@
+class Road{
+    constructor(x, width, lanecount=3, ctx) {
+        this.x = x;
+        this.width = width;
+        this.laneCount = lanecount;
+        this.ctx = ctx;
+
+        this.left = x - width/2;
+        this.right = x + width/2;
+
+        const infinity = 1000000;
+        this.top = -infinity;
+        this.bottom = infinity;
+
+        const topLeft = {x: this.left, y:this.top};
+        const bottomLeft = {x: this.left, y:this.bottom};
+        const topRight = {x: this.right, y:this.top};
+        const bottomRight = {x: this.right, y:this.bottom};
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ];
+    }
+
+    gateLaneCenter(index) {
+        const {width, laneCount, left} = this;
+        const laneWidth = width/laneCount;
+        return left + (1/2 + Math.min(index, laneCount - 1)) * laneWidth;
+    }
+
+    draw() {
+        const {ctx, left, right, top, bottom, laneCount, borders} = this;
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "white";
+
+        const lerp = (A, B, t) => (A + (B - A) * t);
+
+        Array.from({ length: laneCount + 1 }).forEach((emptyElem, index) => {
+
+            if (index == 0 || index == laneCount) {
+                return;
+            }
+            ctx.setLineDash([20, 20]);
+            const x = lerp(left, right, index/laneCount);
+            ctx.beginPath();
+            ctx.moveTo(x, top);
+            ctx.lineTo(x, bottom);
+            ctx.stroke();
+        });
+        ctx.setLineDash([]);
+        borders.forEach(border => {
+            ctx.beginPath();
+            ctx.moveTo(border[0].x, border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
+        });
+    }
+}
